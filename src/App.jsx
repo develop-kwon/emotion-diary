a33b9e69c44d911733a579a834e668b221d69364
YuHyun <kyh010316@naver.com>
2025-08-29 21:41:12 +0900

    필터링 적용

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,21 @@ import NotFound from './pages/NotFound';
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2025-08-29").getTime(),
     emotionId: 1,
     content: "1번 일기 내용"
   },
   {
     id: 2,
-    createdDate: new Date().getTime(),
+    createdDate: new Date("2025-08-28").getTime(),
     emotionId: 2,
     content: "2번 일기 내용"
+  },
+  {
+    id: 3,
+    createdDate: new Date("2025-07-28").getTime(),
+    emotionId: 3,
+    content: "3번 일기 내용"
   }
 ];
 
@@ -43,8 +49,8 @@ function reducer(state, action) {
   }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, mockData);
--- src/components/DiaryItem.jsx
+++ src/components/DiaryItem.jsx
@@ -1,19 +1,24 @@
 import { getEmotionImage } from "../util/get-emtion-image";
 import Button from "./Button";
 import "./DiaryItem.css"
+import { useNavigate } from "react-router-dom";
 
-const DiaryItem = () => {
-    const emotionId = 1;
+const DiaryItem = ({ id, createdDate, emotionId, content }) => {
+    const nav = useNavigate();
+
     return (
         <div className="DiaryItem">
-            <div className={`img_section img_section_${emotionId}`}>
+            <div className={`img_section img_section_${emotionId}`}
+                onClick={() => nav(`/diary/${id}`)}>
                 <img src={getEmotionImage(emotionId)} />
             </div>
-            <div className="info_section">
-                <div className="created_date">{new Date().toLocaleDateString()}</div>
-                <div className="content">test</div>
+            <div className="info_section"
+                onClick={() => nav(`/diary/${id}`)}>
+                <div className="created_date">{new Date(createdDate).toLocaleDateString()}</div>
+                <div className="content">{content}</div>
             </div>
-            <div className="button_section">
+            <div className="button_section"
+                onClick={() => nav(`/edit/${id}`)}>
                 <Button text={"수정하기"} />
             </div>
         </div>
